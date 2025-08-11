fc8ff30b990302728baf8cdddc329325ab5103cf
lindyhsu <128658889+lindyhsu@users.noreply.github.com>
2025-08-11 20:26:32 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,26 +3,27 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function() {
+makeCacheMatrix <- function(){
   mat <- NULL
   inv_cache <- NULL
-  
-  setMatrix <- function(x) {
+
+  setMatrix <- function(x){
     mat <<- x
     inv_cache <<- NULL
-  }
+}
   
-  getInverse <- function() {
-    if (is.null(inv_cache)) {
-      inv_cache <<- solve(mat)
+getInverse <- function(){
+  if(is.null(inv_cache)){
+    inv_cache <<- solve(mat)
     }
-    inv_cache
+  inv_cache
   }
-  
   list(setMatrix = setMatrix, getInverse = getInverse)
 }
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...){
   inv_cache <- x$getInverse()
   inv_cache
 }
+
+
